7be42d0c8b3dedb2dd5843483c61b1e5f3c2b55b
ShivappaE <129170653+ShivappaE@users.noreply.github.com>
2023-07-28 23:25:15 +0530

    BaseClass.java

--- BaseClass.java
+++ BaseClass.java
@@ -1,4 +1,4 @@
-package com.trello.qsp.genericutility;
+package com.airtel.qsp.genericutility;
 
 import java.io.IOException;
 
@@ -6,10 +6,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
-public class BaseClass 
+import com.airtel.qsp.pomrepository.LogoutTrello;
+import com.airtel.qsp.pomrepository.TrelloHomePage;
+import com.airtel.qsp.pomrepository.TrelloLoginPage;
+
+public class BaseClass
 {
 	public WebDriver driver;
 	public ExcelUtility excelUtils = new ExcelUtility();
@@ -17,7 +23,7 @@ public class BaseClass
 	public JavaUtility javaUtils =new JavaUtility();
 	public WebDriverUtility webdriverUtils=new WebDriverUtility();
 	
-	@BeforeMethod
+	@BeforeClass
 	public void configBeforeMethod() throws IOException 
 	{
 		String browserName = fileUtils.readCommonData("browser");
@@ -37,8 +43,31 @@ public class BaseClass
 		webdriverUtils.implicitWait(driver);
 		driver.get(fileUtils.readCommonData("url"));
 	}
+	@BeforeMethod
+	public void login() throws Throwable 
+	{
+		TrelloHomePage homePage = new TrelloHomePage(driver);
+		homePage.getLoginOption().click();
+		webdriverUtils.verifyCompleteTitle(driver, driver.getTitle());
+		
+		TrelloLoginPage login = new TrelloLoginPage(driver);
+		webdriverUtils.ActiveElementSendkeys(driver, "username");
+		login.getContinueOption().click();
+		webdriverUtils.verifyCompleteTitle(driver, "Log in to continue - Log in with Atlassian account");
+		webdriverUtils.ActiveElementSendkeys(driver, "password");
+		login.getLoginButton().submit();
+	}
 	
 	@AfterMethod
+	public void logout() 
+	{
+		LogoutTrello logout = new LogoutTrello(driver);
+		logout.getProfileIcon().click();
+		logout.getProfileLogoutButton().click();
+		logout.getLogoutSubmitButton().submit();
+	}
+	
+	@AfterClass
 	public void configAfterMethod()
 	{
 		driver.manage().window().minimize();
@@ -46,5 +75,3 @@ public class BaseClass
 	}
 	
 }
-
-
