06b518ae20b4181d5938931f3460fbdf87ce09f5
ShivappaE <129170653+ShivappaE@users.noreply.github.com>
2023-07-28 23:26:17 +0530

    BaseClass.java

--- BaseClass.java
+++ BaseClass.java
@@ -1,7 +1,6 @@
 package com.airtel.qsp.genericutility;
 
 import java.io.IOException;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -10,7 +9,6 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-
 import com.airtel.qsp.pomrepository.LogoutTrello;
 import com.airtel.qsp.pomrepository.TrelloHomePage;
 import com.airtel.qsp.pomrepository.TrelloLoginPage;
@@ -43,6 +41,7 @@ public class BaseClass
 		webdriverUtils.implicitWait(driver);
 		driver.get(fileUtils.readCommonData("url"));
 	}
+	
 	@BeforeMethod
 	public void login() throws Throwable 
 	{
